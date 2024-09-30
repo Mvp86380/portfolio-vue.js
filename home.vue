f26b19140ddac91719209f5803dbb1e03acda1f0
Mvp86380 <152332162+Mvp86380@users.noreply.github.com>
2024-09-30 13:33:20 +0200

    Add files via upload

---
+++ home.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="home">
+      <section id="presentation">
+        <h1>{{ prenom }} {{ nom }}</h1>
+        <p>Développeur Web passionné par la création d'applications web modernes et dynamiques.</p>
+      </section>
+      <section id="creations">
+        <h2>Mes créations</h2>
+        <div class="projects">
+          <div v-for="project in projects" :key="project.id" class="project" @click="openModal(project)">
+            <img :src="project.image" alt="project.title" />
+            <h3>{{ project.title }}</h3>
+          </div>
+        </div>
+        <ProjectModal :project="selectedProject" v-if="showModal" @close="closeModal" />
+      </section>
+    </div>
+  </template>
+  <script>
+  import ProjectModal from '@/components/ProjectModal.vue';
+  export default {
+    data() {
+      return {
+        prenom: 'Bastien',
+        nom: 'Schittly',
+        projects: [
+          { id: 1, title: 'Projet 1', image: 'project1.png', details: {...} },
+          { id: 2, title: 'Projet 2', image: 'project2.png', details: {...} },
+        ],
+        showModal: false,
+        selectedProject: null,
+      };
+    },
+    methods: {
+      openModal(project) {
+        this.selectedProject = project;
+        this.showModal = true;
+      },
+      closeModal() {
+        this.showModal = false;
+      },
+    },
+    components: {
+      ProjectModal,
+    },
+  };
+  </script>
+  <style scoped>
+  /* Ajoutez des styles pour la présentation et les projets */
+  </style>
+  
---
+++ projet modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="modal" @click.self="close">
+      <div class="modal-content">
+        <h2>{{ project.title }}</h2>
+        <p>Date de création : {{ project.details.date }}</p>
+        <p>Technologies utilisées : {{ project.details.technologies }}</p>
+        <a :href="project.details.link" target="_blank">Voir le projet</a>
+        <a :href="project.details.github" target="_blank">Code source</a>
+        <button @click="close">Fermer</button>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ['project'],
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+  </script>
+  <style scoped>
+  /* Ajoutez des styles pour la modal et son contenu */
+  </style>
+  
